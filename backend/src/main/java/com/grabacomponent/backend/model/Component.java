7b9af300e21c8882ee80fc1266862908bceb8858
Sharvesh <sharveshguru2004@gmail.com>
2025-07-23 19:23:43 +0530

    setup the backend and models

---
+++ backend/src/main/java/com/grabacomponent/backend/model/Component.java
@@ -0,0 +1,32 @@
+package com.grabacomponent.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="components")
+public class Component {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable=false)
+    private String description;
+
+    @Lob
+    private byte[] image;
+    
+}
---
+++ backend/src/main/java/com/grabacomponent/backend/model/Listing.java
@@ -0,0 +1,38 @@
+package com.grabacomponent.backend.model;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "listings")
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "component_id")
+    private Component component;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User seller;
+
+    @Column(nullable = false)
+    private boolean availability = true;
+
+    @Column(nullable = false)
+    private int quantity;
+}
+

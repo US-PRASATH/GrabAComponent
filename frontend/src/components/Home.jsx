62ec121c98d7c8aa652f8d6eb8e02ce31a3e537c
Sharvesh <sharveshguru2004@gmail.com>
2025-07-29 09:48:38 +0530

    initialize frontend

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="bg-[#181C14] h-screen">
+            <h1 className="text-3xl">Grab A Component!</h1>
+        </div>
+    )
+}
+export default Home;

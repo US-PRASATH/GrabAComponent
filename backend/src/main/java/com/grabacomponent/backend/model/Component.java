52d045d8ad0664d4c17f384eb9f950b6d3fdbaff
Sharvesh <sharveshguru2004@gmail.com>
2025-08-02 12:30:24 +0530

    change primary key generation type to identity

--- backend/src/main/java/com/grabacomponent/backend/model/Component.java
+++ backend/src/main/java/com/grabacomponent/backend/model/Component.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Table(name="components")
 public class Component {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
